6f880be321c082b09b4591f688bb51639d48f6a5
khanshorif331 <khanshorif331@gmail.com>
2023-01-11 00:34:17 +0600

    middleware function done

---
+++ src/redux/middlewares/cartCounter.js
@@ -0,0 +1,7 @@
+// --------curry function
+const cartCounter = store => next => action => {
+	console.log('Current State', store.getState())
+	console.log('Action', action)
+	return next(action)
+}
+export default cartCounter
